46851c756d55e52256b70adfd6136fb59b316cc1
SUHONG\topic <topic828@Naver.com>
2024-05-13 22:31:10 +0900

    firebase 설정 및 useSession을 통한 유저정보 가져오기

--- app/(enforcement)/page.tsx
+++ app/(enforcement)/page.tsx
@@ -2,17 +2,20 @@ import { auth } from '@/auth';
 import EnforceScene from '@/features/enforcement/EnforceScene';
 import CanvasLayout from '@/shared/components/3dmodel';
 import Header from '@/shared/components/Header';
+import { SessionProvider } from 'next-auth/react';
 
-export default async function Home() {
+export default async function Home({}) {
   const session = await auth();
 
   return (
     <main>
       <Header />
       <div>{JSON.stringify(session, null, 2)}</div>
-      <CanvasLayout>
-        <EnforceScene />
-      </CanvasLayout>
+      <SessionProvider>
+        <CanvasLayout>
+          <EnforceScene />
+        </CanvasLayout>
+      </SessionProvider>
     </main>
   );
 }
