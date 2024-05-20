a96ae677350b710e37551fa5c6ac9e36f79dd5b8
SUHONG\topic <topic828@Naver.com>
2024-05-20 14:00:16 +0900

    popup을 통해 usermenu생성

--- shared/components/header/auth/index.tsx
+++ shared/components/header/auth/index.tsx
@@ -1,17 +1,10 @@
 import { auth } from '@/auth';
-import { SignIn } from './AuthComponent';
-import Image from 'next/image';
+import { SignIn } from './SignIn';
+import UserMenu from './UserMenu';
 
 export default async function AuthButton() {
   const session = await auth();
   if (!session?.user) return <SignIn />;
 
-  return (
-    <Image
-      src={session.user.image!}
-      alt="내구글이미지"
-      width={50}
-      height={50}
-    />
-  );
+  return <UserMenu image={session.user.image!} />;
 }
