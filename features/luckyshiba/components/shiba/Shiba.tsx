d2277f7e1fe71348c8ad2b41475e84f7daa5b01d
SUHONG\topic <topic828@Naver.com>
2024-06-14 06:18:39 +0900

    포지션 훅 개선을 통해 끊김현상 감소 및 MoveCar 컴퍼넌트 분리

--- features/luckyshiba/components/shiba/Shiba.tsx
+++ features/luckyshiba/components/shiba/Shiba.tsx
@@ -6,9 +6,9 @@ import { GLTF } from 'three-stdlib';
 import { useCompoundBody, useRaycastVehicle } from '@react-three/cannon';
 import { useFrame } from '@react-three/fiber';
 import { useFollowCam } from '@/shared/hooks/useFollowCam';
-import { useWheels } from '../../hooks/useWheels';
-import { useMoveCar } from '../../hooks/useMoveCar';
+
 import { useMovePosition } from '../../hooks/useMovePosition';
+
 type GLTFResult = GLTF & {
   nodes: {
     Group18985_default_0: Mesh;
@@ -33,7 +33,7 @@ export function Shiba() {
   const worldPosition = useMemo(() => new Vector3(), []);
   const worldDirection = useMemo(() => new Vector3(), []);
 
-  const position: [x: number, y: number, z: number] = [10, 8, 2];
+  const position: [x: number, y: number, z: number] = [0, 1, 0];
 
   const width = 0.65;
   const height = 1.2;
@@ -46,7 +46,7 @@ export function Shiba() {
     () => ({
       position,
       mass: mass,
-      rotation: [0, -Math.PI / 2, 0],
+      rotation: [0, 0, 0],
       collisionFilterGroup: 5,
       shapes: [
         {
@@ -59,18 +59,12 @@ export function Shiba() {
     useRef<Group>(null)
   );
 
-  // const [wheels, wheelInfos] = useWheels({ width, height, front });
-  // const [vehicle, vehicleApi] = useRaycastVehicle(
-  //   () => ({
-  //     chassisBody,
-  //     wheelInfos,
-  //     wheels,
-  //   }),
-  //   useRef<Group>(null)
-  // );
-
-  // useMoveCar({ chassisApi, vehicleApi, worldDirection });
-  useMovePosition({ worldDirection, worldPosition, chassisApi });
+  const controlMovement = useMovePosition({
+    worldDirection,
+    worldPosition,
+    chassisApi,
+    chassisBody,
+  });
 
   const makeFollowCam = () => {
     chassisBody?.current!.getWorldPosition(worldPosition);
@@ -80,13 +74,13 @@ export function Shiba() {
 
   useFrame((_, delta) => {
     makeFollowCam();
+    controlMovement(delta);
   });
 
   return (
-    // <group ref={vehicle}>
     <group>
       <group ref={chassisBody} position={[0, 0.5, 20]} castShadow>
-        <group position={[0, 0.35, -0.5]} rotation={[-Math.PI / 2, 0, Math.PI]}>
+        <group position={[0, 0.35, 0.5]} rotation={[-Math.PI / 2, 0, 0]}>
           <mesh
             geometry={nodes.Group18985_default_0.geometry}
             material={materials['default']}
--- features/luckyshiba/hooks/useMovePosition.ts
+++ features/luckyshiba/hooks/useMovePosition.ts
@@ -1,4 +1,4 @@
-import { Quaternion, Vector3 } from 'three';
+import { Group, Object3DEventMap, Quaternion, Vector3 } from 'three';
 import { useInput } from './useInput';
 import { PublicApi } from '@react-three/cannon';
 import { useMemo } from 'react';
@@ -7,22 +7,23 @@ type MovePositionProps = {
   worldPosition: Vector3;
   worldDirection: Vector3;
   chassisApi: PublicApi;
+  chassisBody: React.RefObject<Group<Object3DEventMap>>;
 };
 
 export const useMovePosition = ({
   worldDirection,
   worldPosition,
   chassisApi,
+  chassisBody,
 }: MovePositionProps) => {
   const { forward, backward, left, right, jump, stand } = useInput();
   const worldQuaternion = useMemo(() => new Quaternion(), []);
 
   const controlMovement = (delta: number) => {
     if (forward || backward) {
-      const speed = delta * 2;
+      const speed = delta * 4;
       let { x, y, z } = worldPosition;
       let { x: rx, y: ry, z: rz } = worldDirection;
-
       let [newX, newY, newZ] = [x, y, z];
 
       if (forward) {
@@ -33,12 +34,13 @@ export const useMovePosition = ({
         newX -= rx * speed;
         newZ -= rz * speed;
       }
-      chassisApi.position.set(newX, 0, newZ);
+      chassisApi.position.set(newX, newY, newZ);
     }
 
     if (right || left) {
       const turnAngle = delta;
       const turnQuaternion = new Quaternion();
+      chassisBody?.current!.getWorldPosition(worldPosition);
 
       if (right) {
         turnQuaternion.setFromAxisAngle(new Vector3(0, 1, 0), -turnAngle);
@@ -60,7 +62,5 @@ export const useMovePosition = ({
     }
   };
 
-  useFrame((_, delta) => {
-    controlMovement(delta);
-  });
+  return controlMovement;
 };
