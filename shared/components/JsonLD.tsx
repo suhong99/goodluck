6cb597e05d754e150c819176d1f318a00c03c1a8
SUHONG\topic <topic828@Naver.com>
2024-07-14 15:07:07 +0900

    item 추가, 값과 데이터 분리

--- shared/components/JsonLD.tsx
+++ shared/components/JsonLD.tsx
@@ -1,26 +1,24 @@
 export default function JSONLD() {
+  const data: { name: string; url: string }[] = [
+    { name: '모의 강화하기', url: 'https://goodluck-steel.vercel.app/' },
+    {
+      name: '운좋은 시바',
+      url: 'https://goodluck-steel.vercel.app/luckyshiba',
+    },
+  ];
+
   const jsonLd = {
     '@context': 'https://schema.org',
     '@type': 'BreadcrumbList',
-    itemListElement: [
-      {
-        '@type': 'ListItem',
-        position: 1,
-        item: {
-          '@id': 'https://goodluck-steel.vercel.app/',
-          name: '모의 강화하기',
-        },
-      },
-      {
-        '@type': 'ListItem',
-        position: 2,
-        item: {
-          '@id': 'https://goodluck-steel.vercel.app/luckyshiba',
-          name: '운좋은 시바',
-        },
-      },
-    ],
+    itemListElement: data.map(({ name, url }, idx) => ({
+      '@type': 'ListItem',
+      position: idx + 1,
+      name,
+      '@id': url,
+      item: url,
+    })),
   };
+
   return (
     <script
       type="application/ld+json"
